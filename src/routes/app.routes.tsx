ac56a2e25e99284d28d6f608d7d170d7776d56a8
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-24 19:51:38 -0300

    fix: add app routes base code

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Route, Routes, Navigate, redirect } from "react-router-dom";
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+    )
+}
