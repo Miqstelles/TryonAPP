d091eeb108253544e5a60d476499152694dc41e4
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 19:09:27 -0300

    feat: update button styles

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -2,19 +2,21 @@ import { StyleSheet } from 'react-native';
 import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
     button: {
         backgroundColor: THEME.COLORS.BACKGROUND_100,
         padding: 10,
         borderRadius: 5,
+        width: 155,
+        height: 45,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
     },
     buttonText: {
         color: THEME.COLORS.CAPTION_500,
-        fontSize: 16,
+        fontSize: 20,
+        fontFamily: THEME.FONT_FAMILY.BOLD,
         textAlign: 'center',
     },
 });
