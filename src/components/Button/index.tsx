f35ebee68c3c4bf47ddb93de1d6fe08dec6edacb
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 19:30:07 -0300

    feat: update button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 import { TouchableOpacity, StyleSheet, Text, TouchableOpacityProps } from 'react-native';
 
 import { styles } from './style';
+import { THEME } from '../../theme';
 
 interface ButtonProps extends TouchableOpacityProps {
     title: string;
+    size: number;
 }
 
-export function Button({ title, ...rest }: ButtonProps) {
+export function Button({ title, size = 50, ...rest }: ButtonProps) {
+    const buttonStyles = StyleSheet.create({
+        button: {
+            backgroundColor: THEME.COLORS.BACKGROUND_100,
+            padding: 10,
+            borderRadius: 5,
+            width: size,
+            height: 45,
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 4 },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+        },
+    })
     return (
-        <TouchableOpacity style={styles.button} {...rest}>
+        <TouchableOpacity style={buttonStyles.button} {...rest}>
             <Text style={styles.buttonText}>{title}</Text>
         </TouchableOpacity>
     );
--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -2,17 +2,6 @@ import { StyleSheet } from 'react-native';
 import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
-    button: {
-        backgroundColor: THEME.COLORS.BACKGROUND_100,
-        padding: 10,
-        borderRadius: 5,
-        width: 155,
-        height: 45,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-    },
     buttonText: {
         color: THEME.COLORS.CAPTION_500,
         fontSize: 20,
