31be58217c566f519f54a0eb93498363a0d31923
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 18:47:27 -0300

    feat: add logo and slogan in loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,20 @@
-import { View, ActivityIndicator } from 'react-native';
-import { THEME } from '../../theme';
-
+import logoImg from '../../assets/logo.png'
 import { styles } from './style';
+import { Background } from '../Background';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Heading } from '../Heading';
+import { ActivityIndicator, Image, View } from "react-native";
+import { THEME } from '../../theme';
 
 export function Loading() {
     return (
         <View style={styles.container}>
             <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+            <Image
+                style={styles.logo}
+                source={logoImg}
+            />
+            <Heading />
         </View>
     );
 }
