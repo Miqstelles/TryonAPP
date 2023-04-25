81b9185babf8209527a4af2333e2bc189ee96a7d
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 19:30:44 -0300

    feat: update home styles

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -1,12 +1,24 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        gap: 40
     },
     logo: {
         marginBottom: 28
+    },
+    title: {
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        fontSize: 24,
+    },
+    btn: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '80%',
+        marginTop: 20,
     }
 });
