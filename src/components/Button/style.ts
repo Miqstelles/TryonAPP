1846064f745a6fe6ed0b24c3b21f98b523e381c7
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 18:46:56 -0300

    feat: add button component

---
+++ src/components/Button/style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: '#F3D9BB',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#CB8940',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+});
