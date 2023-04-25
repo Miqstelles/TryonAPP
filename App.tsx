779dbfd7892e1ee53a656fc1d880e421f6beab5b
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 04:29:22 -0300

    feat: add Knewave font family

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Raleway_300Light, Raleway_400Regular, Raleway_700Bold } from '@expo-google-fonts/raleway'
+import { Knewave_400Regular } from '@expo-google-fonts/knewave'
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Raleway_400Regular, Raleway_300Light, Raleway_700Bold
+    Raleway_400Regular, Raleway_300Light, Raleway_700Bold, Knewave_400Regular
   });
 
   return (
