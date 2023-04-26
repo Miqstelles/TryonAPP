ac304205b4df1cff7cf81c7c368a48cd90d35748
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-26 00:17:20 -0300

    feat: update input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,17 +3,18 @@ import { TextInput, TextInputProps } from 'react-native';
 import { styles } from './style';
 
 interface InputProps extends TextInputProps {
+    value: string;
+    onChangeText: (text: string) => void;
 }
 
-export function Input({ ...rest }: InputProps) {
-    const [text, onChangeText] = React.useState('');
-
+export function Input({ value, onChangeText, ...rest }: InputProps) {
     return (
         <TextInput
             style={styles.input}
-            onChangeText={onChangeText}
-            value={text}
             placeholder='Insira o ingrediente'
+            value={value}
+            onChangeText={onChangeText}
+            {...rest}
         />
     );
 }
