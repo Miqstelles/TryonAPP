ba5fab23f970cdf23572411651578c2aa44e9306
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 22:32:43 -0300

    feat: add recipes screen component

---
+++ src/screens/Recipes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './style';
+
+export function Recipes() {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
