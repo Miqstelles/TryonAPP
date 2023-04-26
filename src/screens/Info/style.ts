ad613037ec9fce3bb5fcecd2676333dc1570ef6b
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-26 20:55:07 -0300

    feat: add info component styles

--- src/screens/Info/style.ts
+++ src/screens/Info/style.ts
@@ -1,5 +1,28 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
-    container: {}
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 50,
+    },
+    howToUse: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 50
+    },
+    infos: {
+        gap: 50
+    },
+    info: {
+        fontSize: 24,
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+        width: 306,
+    },
+    title: {
+        fontSize: 32,
+        fontFamily: THEME.FONT_FAMILY.REGULAR,
+    }
 });
