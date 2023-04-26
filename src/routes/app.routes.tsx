88d19b8fbde6ce5874066d9782ce786e9534fc7e
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-25 22:32:22 -0300

    feat: add bottom tab navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,40 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HouseSimple, Info as InfoIcon, Hamburger } from 'phosphor-react-native';
 import { Home } from '../screens/Home'
+import { Info } from '../screens/Info';
+import { Recipes } from '../screens/Recipes';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
     return (
-        <Navigator screenOptions={{ headerShown: false }}>
+        <Navigator screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+                backgroundColor: '#E6CCAE', shadowColor: '#000',
+                shadowOffset: { width: 0, height: 4 },
+                shadowOpacity: 0.25,
+                shadowRadius: 4,
+                height: 80,
+                padding: 32,
+            },
+            tabBarLabel: '',
+
+        }}>
             <Screen
                 name="home"
                 component={Home}
+                options={{ tabBarIcon: ({ focused }) => (<HouseSimple color={focused ? '#fff' : '#000'} size={32} />) }}
+            />
+            <Screen
+                name="info"
+                component={Info}
+                options={{ tabBarIcon: ({ focused }) => (<InfoIcon color={focused ? '#fff' : '#000'} size={32} />) }}
+            />
+            <Screen
+                name="recipes"
+                component={Recipes}
+                options={{ tabBarIcon: ({ focused }) => (<Hamburger color={focused ? '#fff' : '#000'} size={32} />) }}
             />
         </Navigator>
     )
