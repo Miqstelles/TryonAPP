a0cb2416f6700d970dbf7ecca30651adc8ccca54
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-26 00:19:03 -0300

    feat: add background in info screen

--- src/screens/Info/index.tsx
+++ src/screens/Info/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { styles } from './style';
+import { Background } from '../../components/Background';
 
 export function Info() {
     return (
-        <View style={styles.container}>
+        <Background>
 
-        </View>
+        </Background>
     );
 }
