abe40bca0c4442a8de86a4864c9d219216d3ac70
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-04-27 19:47:50 -0300

    feat: add componentes in recipe screen and add function to search recipe by name

--- src/screens/Recipes/index.tsx
+++ src/screens/Recipes/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { CaretRight } from 'phosphor-react-native';
+
+import { Background } from '../../components/Background';
+import { Input } from '../../components/Input';
+import recipesData from '../../../receitas.json';
 
 import { styles } from './style';
-import { Background } from '../../components/Background';
+import { THEME } from '../../theme';
 
 export function Recipes() {
+    const [recipe, setRecipe] = useState('');
+
+    const searchRecipes = () => {
+        const recipesArray = Object.values(recipesData);
+        return recipesArray.filter(item => item.nome.toLowerCase().includes(recipe.toLowerCase()));
+    }
+
     return (
         <Background>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.title}>DIGITE O NOME DA RECEITA</Text>
+                <Input
+                    value={recipe}
+                    onChangeText={setRecipe}
+                />
 
+                {searchRecipes().length > 0 && recipe && recipe != ' ' ?
+                    <ScrollView>
+                        <View style={styles.recipeList}>
+                            {searchRecipes().map((item, index) => (
+                                <View style={styles.recipeItem} key={index}>
+                                    <Text style={styles.recipeText}>{index + 1}. {item.nome}</Text>
+                                    <CaretRight size={44} />
+                                </View>
+                            ))}
+                        </View>
+                    </ScrollView>
+                    :
+                    <Text style={{ fontSize: 18, width: '80%', fontFamily: THEME.FONT_FAMILY.LIGHT }}>Digite o nome de uma receita para pesquisar.</Text>
+                }
+
+            </SafeAreaView>
         </Background>
     );
 }
