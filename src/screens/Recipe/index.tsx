584e1fa5ff49219213cae5f68691a2b4410243e2
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-02 21:12:13 -0300

    feat: add recipe screen component

--- src/screens/Recipe/index.tsx
+++ src/screens/Recipe/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { styles } from './style';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { Background } from '../../components/Background';
 
-export function Recipe() {
+interface RecipeDetailsRouteParams {
+    nome: string;
+    ingredientes: string[];
+    modoPreparo: string[];
+}
+
+export function RecipeDetails() {
+    const route = useRoute<RouteProp<Record<string, RecipeDetailsRouteParams>, string>>();
+    const { nome, ingredientes, modoPreparo } = route.params;
+
+    const [option, setOption] = useState('')
+
     return (
-        <View style={styles.container}>
+        <Background>
+            <View style={styles.container}>
+                <Text style={styles.title}>{nome}</Text>
 
-        </View>
+                <View style={styles.options}>
+                    <Text style={option == 'ingrediente' ? styles.optionsTextSelected : styles.optionsText} onPress={() => setOption('ingrediente')}>Ingredientes</Text>
+                    <Text style={option == 'preparo' ? styles.optionsTextSelected : styles.optionsText} onPress={() => setOption('preparo')}>Preparo</Text>
+                </View>
+
+                <View>
+                    <ScrollView style={{ maxHeight: 282 }} >
+                        {option === 'ingrediente' ?
+                            <Text style={styles.ingrediente}>{ingredientes.join(', ').replace(/,/g, ',\n')}</Text>
+                            :
+                            <Text style={styles.ingrediente}>{modoPreparo.join('\n')}</Text>
+                        }
+                    </ScrollView>
+                </View>
+
+            </View >
+        </Background>
     );
 }
