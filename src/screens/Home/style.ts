f9af9fc4538acd2bf816fc360106ec4b00162d78
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-03 21:23:48 -0300

    feat: update home component styles

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -6,7 +6,8 @@ export const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        gap: 40
+        gap: 40,
+        marginTop: 20
     },
     logo: {
         marginBottom: 28
@@ -34,5 +35,29 @@ export const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginTop: 24,
+    },
+    recipeList: {
+        backgroundColor: THEME.COLORS.BACKGROUND_100,
+        paddingHorizontal: 20,
+        paddingTop: 20,
+        gap: 20,
+        minWidth: '100%', shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+    },
+    recipeItem: {
+        marginBottom: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    recipeText: {
+        fontSize: 20,
+        fontFamily: THEME.FONT_FAMILY.BOLD,
+        width: '80%'
     }
 });
